2e799bff005866cbc8b8803aa97d39e338f88f38
Joseph Baca <josephcbaca@gmail.com>
2020-07-01 20:54:40 -0700

    Update members.html and members.js to include a quick input form for testing.

--- public/js/members.js
+++ public/js/members.js
@@ -5,3 +5,27 @@ $(document).ready(() => {
     $(".member-name").text(data.email);
   });
 });
+
+// Input Group
+let breweryDiv = $("<div>");
+let formGroupDiv = $("<div>");
+let brewerySearchInput = $("<input>");
+let brewerysearchInput = $("<button>");
+
+breweryDiv.addClass("form-group searchDiv");
+breweryDiv.text("Brewery Search");
+
+formGroupDiv.addClass("row-fluid searchDiv");
+
+brewerySearchInput.addClass("form-control");
+brewerySearchInput.attr("id", "foodType");
+brewerySearchInput.attr("placeholder", "Type of Food...");
+
+brewerysearchInput.addClass("btn");
+brewerysearchInput.attr("id", "search-button");
+brewerysearchInput.text("Search");
+
+$(".profileDiv").append(breweryDiv);
+$(".form-group").append(formGroupDiv);
+$(".row-fluid").append(brewerySearchInput);
+$(".form-group").append(brewerysearchInput);
