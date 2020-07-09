02b1210f526bd2b08e991b7662117a3e502f8cba
JMan <60026263+JMantis0@users.noreply.github.com>
2020-07-09 05:06:33 -0700

    Added features that require a user to select an account type at signup.  User table in database now has usertype column.  Routes adjusted to route users to the correct page based on their usertype.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -12,7 +12,8 @@ module.exports = function(app) {
     // Sending back a password, even a hashed password, isn't a good idea
     res.json({
       email: req.user.email,
-      id: req.user.id
+      id: req.user.id,
+      usertype: req.user.usertype
     });
   });
 
@@ -22,7 +23,8 @@ module.exports = function(app) {
   app.post("/api/signup", (req, res) => {
     db.User.create({
       email: req.body.email,
-      password: req.body.password
+      password: req.body.password,
+      usertype: req.body.usertype
     })
       .then(() => {
         res.redirect(307, "/api/login");
