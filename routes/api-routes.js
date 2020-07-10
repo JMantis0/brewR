3b11b0b84d00781811a9a3b58a5eaf6333b59b95
JMan <60026263+JMantis0@users.noreply.github.com>
2020-07-10 15:41:19 -0700

    Removed unused category code from feed.js.  Also added to app.get("/api/posts/" route to get only blog posts for current user via foreign key

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -101,11 +101,18 @@ module.exports = function(app) {
   //     res.json(data);
   //   });
   // });
+
+  
   // blog posts crud starts here
   // GET route for getting all of the posts
   app.get("/api/posts/", (req, res) => {
-    db.Post.findAll({}).then(data => {
-      res.json(data);
+    //Get all posts that belong to the current user
+    db.Post.findAll({
+      where: {
+        UserID: req.user.id
+      }
+    }).then(userposts => {
+      res.json(userposts);
     });
   });
 
