8daaebf268e53c0dd5043404e71ace68058e1aef
JMan <60026263+JMantis0@users.noreply.github.com>
2020-07-10 01:04:18 -0700

    removed some console.logs();

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -9,7 +9,6 @@ module.exports = function(app) {
   app.get("/", (req, res) => {
     // If the user already has an account send them to the members page
     if (req.user) {
-      console.log(req.user)
       res.redirect("/members");
     }
     res.render("login");
