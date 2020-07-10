35c79e852b6b83a311cfe1f97667bdb4df841132
JMan <60026263+JMantis0@users.noreply.github.com>
2020-07-10 01:02:10 -0700

    Added to .then() callback of lines 31-39 of login.js to direct different account types to the correct pages, wheras before brewRys were being sent to /members

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -10,6 +10,7 @@ module.exports = function(app) {
   // Otherwise the user will be sent an error
   app.post("/api/login", passport.authenticate("local"), (req, res) => {
     // Sending back a password, even a hashed password, isn't a good idea
+    console.log(req);
     res.json({
       email: req.user.email,
       id: req.user.id,
