838a1c6681616d0a9c507c5288b95258a7502148
Joseph Baca <josephcbaca@gmail.com>
2020-07-11 14:14:28 -0700

    Updated Schema.

--- db/schema.sql
+++ db/schema.sql
@@ -4,23 +4,50 @@ CREATE DATABASE brewr;
 
 USE brewr;
 
--- members table is for blog posts
-CREATE TABLE members (
+CREATE TABLE brewerybeers(
 	id int NOT NULL AUTO_INCREMENT,
-    post VARCHAR (255) NOT NULL, 
+	beername VARCHAR(255) NOT NULL,
+    beerstyle VARCHAR(255) NOT NULL,
+    beerabv DECIMAL(10) NOT NULL,
+    beerhops VARCHAR(10) NOT NULL,
+    createdAt DATETIME NOT NULL,
+    updatedAt DATETIME NOT NULL,
 	PRIMARY KEY (id)
 );
 
-CREATE TABLE brewery_name(
+CREATE TABLE posts (
 	id int NOT NULL AUTO_INCREMENT,
-	brewery_name VARCHAR(255) NOT NULL,
-    brewery_address VARCHAR(255) NOT NULL,
-    brewery_state VARCHAR(255) NOT NULL,
-    brewery_zip VARCHAR(10) NOT NULL,
-    contact_phone VARCHAR(20) NOT NULL,
-    contact_email VARCHAR(255) NOT NULL,
-	PRIMARY KEY (id)
+    body VARCHAR(255) NOT NULL, 
+	createdAt DATETIME NOT NULL,
+    updatedAt DATETIME NOT NULL,
+    UserID INT(10) NOT NULL,
+    PRIMARY KEY (id)
 );
 
+CREATE TABLE users (
+	id int NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL, 
+    password VARCHAR(255) NOT NULL, 
+	usertype VARCHAR(255) NOT NULL, 
+    createdAt DATETIME NOT NULL,
+    updatedAt DATETIME NOT NULL,
+    UserID INT(10) NOT NULL,
+    PRIMARY KEY (id)
+);
 
-
+CREATE TABLE faves (
+	id int NOT NULL AUTO_INCREMENT,
+    brewer_id VARCHAR(255) NOT NULL, 
+    name VARCHAR(255) NOT NULL, 
+	brewery_type VARCHAR(255) NOT NULL,
+	street VARCHAR(255) NOT NULL,
+    city VARCHAR(255) NOT NULL,
+    state VARCHAR(255) NOT NULL,
+    postal_code VARCHAR(255) NOT NULL,
+    country VARCHAR(255) NOT NULL,
+    phone VARCHAR(255) NOT NULL,
+    website VARCHAR(255) NOT NULL,
+    createdAt DATETIME NOT NULL,
+    updatedAt DATETIME NOT NULL,
+    PRIMARY KEY (id)
+);
